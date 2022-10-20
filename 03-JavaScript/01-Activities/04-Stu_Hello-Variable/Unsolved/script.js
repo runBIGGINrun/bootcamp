5facf609894b4ac3b227434b8bbdab122783827d
runBIGGINrun <114839299+runBIGGINrun@users.noreply.github.com>
2022-10-19 21:46:39 -0500

    complete activitity 2, 4

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,17 @@
-// Write Your JavaScript Code Here
+
+var personName = 'Neil';
+var pets = '6';
+var funFact = 'I love cheese!';
+
+console.log('my name is ' + (personName));
+console.log('I have ' + (pets) + ' great pets.');
+console.log('Here is a fun fact, ' + (funFact));
+
+personName = 'Biggin';
+pets = 'too many';
+funFact = 'I\'m bald.'; 
+
+
+console.log('my name is ' + (personName));
+console.log('I have ' + (pets) + ' great pets.');
+console.log('Here is a fun fact, ' + (funFact));
